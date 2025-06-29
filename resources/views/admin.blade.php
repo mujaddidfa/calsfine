e89371637655032b08b599fa5520be01f498f292
Mujaddid Fathi Athoillah <mujaddid201@gmail.com>
2025-06-30 01:51:20 +0700

    first commit

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Page</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="text-3xl font-bold text-blue-600">
+    Hello World!
+</body>
+</html>
